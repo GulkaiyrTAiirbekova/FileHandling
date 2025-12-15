2fcd48e2038de08b7c1120d3722230cd2fc5d3d2
Gulkaiyr Taiirbekova <taiirbekovag@gmail.com>
2025-12-15 20:19:18 +0600

    git commit -m "Add character stream for reading from input file and writing to output file"

---
+++ src/CharacterStream.java
@@ -0,0 +1,35 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CharacterStream {
+    public static void main(String[] args) {
+
+        FileReader inputStream = null;
+        FileWriter outputStream = null;
+
+        try {
+            inputStream = new FileReader("xanadu.txt");
+            outputStream = new FileWriter("characteroutput.txt");
+
+            int c;
+            while ((c = inputStream.read()) != -1) {
+                outputStream.write(c);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+                if (outputStream != null) {
+                    outputStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
