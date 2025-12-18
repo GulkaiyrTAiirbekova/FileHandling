fb90a926a757937a120ef36f8df3b712e5dbba68
Gulkaiyr Taiirbekova <taiirbekovag@gmail.com>
2025-12-18 18:13:37 -0500

    feat(HW1): define generic IRepository interface
    - Created IRepository<T> interface with abstract methods: getAll, getById, add, update, remove
    - Ensures consistent CRUD contract for all repository implementations

---
+++ src/HW1/IRepository.java
@@ -0,0 +1,10 @@
+package HW1;
+import java.util.List;
+
+public interface IRepository<T> {
+    List<T> getAll();
+    T getById(int id);
+    void add(T entity);
+    void update(T entity);
+    void remove(int id);
+}
