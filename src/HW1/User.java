860d0957e2987d3b7e9bc067bc8b3b96120cc5a8
Gulkaiyr Taiirbekova <taiirbekovag@gmail.com>
2025-12-18 18:14:22 -0500

    feat(HW1): add User entity with transient password field
    - Created User class with fields: id, name, email, password
    - Marked 'password' as transient to avoid file serialization
    - Implemented getters, setters, and toString() for convenient access

---
+++ src/HW1/User.java
@@ -0,0 +1,45 @@
+package HW1;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+
+    private int id;
+    private String name;
+    private String email;
+    private transient String password; //won't be saved to file
+
+    public User(int id, String name,String email,String password){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    //Getters and Setters
+
+    public int getId(){return id;}
+    public void setId(int id){this.id = id;}
+
+    public String getName(){return name;}
+    public void setName(String name){this.name = name;}
+
+    public String getEmail(){return email;}
+    public void setEmail(String email){ this.email = email;}
+
+    public String getPassword(){return password;}
+    public void setPassword(String password){this.password = password;}
+
+
+    @Override
+    public String toString() {
+        return "User{ " +
+                "id = " +
+                ",email = ' " + email + '\'' +
+                ", password= ' " + password + '\'' +
+                '}';
+
+    }
+}
